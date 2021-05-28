d6e4858b852f6372586debf036732c8ed3df575d
Mehmet DAĞDEVİREN <mehmet.dagdeviren@gmail.com>
2021-05-28 16:55:19 +0300

    280520211655

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,3 +1,6 @@
+import 'package:coinfav/views/news.dart';
+import 'package:coinfav/views/portfolio.dart';
+import 'package:coinfav/views/settings.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -6,13 +9,40 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int _currentIndex = 1;
+  final List<Widget> _children = [NewsPage(), PortfolioPage(), SettingsPage()];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Coin Fav'),
+      // appBar: AppBar(
+      //   title: Text('Coin Fav'),
+      // ),
+      body: _children[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.rss_feed),
+            label: 'Haberler',
+            backgroundColor: Colors.red,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            label: 'Portföy',
+            backgroundColor: Colors.green,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Ayarlar',
+            backgroundColor: Colors.pink,
+          ),
+        ],
+        currentIndex: _currentIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: (value) => setState(() {
+          _currentIndex = value;
+        }),
       ),
-      body: Container(),
     );
   }
 }
